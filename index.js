f41ebe00c778271ec8e87847bd56c5251e80fb51
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-04-06 15:58:03 +0300

    second commit

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ let car = new Car('CHERY','Arrizo8',2022)
 car.startEngine()
 car.getInfo()
 
+
 function Student(name, group) {
     this.name=name
     this.group=group
@@ -42,3 +43,49 @@ student.addGrade(prompt('Введите название предмета'),+pro
 student.addGrade(prompt('Введите название предмета'),+prompt("Введите оценку"))
 console.log(student.getAverage())
 student.getInfo()
+
+
+function BankAccount(owner, accountNumber=Math.floor(Math.random()*(20-1+1))+1, balance=0) {
+    this.owner=owner
+    this.balance=balance
+    this.accountNumber=accountNumber
+    this.deposit=function(amount) {
+        return this.balance+amount
+    }
+    this.withdraw=function(amount1) {
+        if (amount1>this.balance) {
+            console.log('На счету не достаточно денег')
+        } else {
+           return this.balance-amount1
+        }
+    }
+    this.getAccountInfo=function() {
+        console.log(this.owner, this.accountNumber, this.balance)
+    }
+}
+let bankAccount = new BankAccount('Artem')
+bankAccount.withdraw(100)
+bankAccount.deposit(+prompt('Введите сумму'))
+bankAccount.withdraw(100)
+bankAccount.getAccountInfo()
+
+
+function Product(name, price, discount=0) {
+    this.name=name
+    this.price=price
+    this.discount=discount
+    this.getFinalPrice=function() {
+        return this.price-this.discount
+    }
+    this.setDiscount=function(newDiscount){
+        this.discount==(this.price*newDiscount)/100
+    }
+    this.getInfo=function() {
+        return console.log(this.name,this.price,this.discount)
+    }
+}
+let product=new Product('Колбаса',100)
+product.getFinalPrice()
+product.setDiscount(+prompt('Введите скидку'))
+product.getFinalPrice()
+product.getInfo()
